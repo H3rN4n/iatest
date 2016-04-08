191f9c323822fd7689e83ad095590fc8e0bf3bdc
H3rN4n <desouza030689@gmail.com>
2016-04-08 02:59:37 -0300

    mapUserTags

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,24 +9,42 @@
  */
 angular.module('iamatApp')
   .controller('MainCtrl', function ($scope, iamatService ,resultsFactory,googleChartApiPromise) {
-    
-    $scope.results = resultsFactory;
 
-    function getFirst(obj){
-    	for (first in obj) break;
-    	return first
-    }
+    function generateTags(obj){
 
-    
+	    Object.keys(obj).forEach(function (key) {
+	        var temp = $scope.results[key];
 
-    function buildDataTable(){
-        $scope.chartObject.data = new google.visualization.DataTable();
-        // Continue building the data table.
+	        temp.key = key;
+
+	        temp.team = false;
+
+	        var name = key;
+
+	        if (key.indexOf('team-') > -1) {
+	            temp.team = true;
+	            var temp2 = key.replace('room:team-','');
+	            for (var i = teamsData.length - 1; i >= 0; i--) {
+	                if (teamsData[i]._id == temp2) {
+	                    name = teamsData[i].name;
+	                    break;
+	                }
+	            };
+	        }
+
+	        //temp.name = name.replace('room:','');
+
+	        //temp.total = temp.answers.sum();
+	        //temp.max = temp.answers.maximum();
+	        //temp.min = temp.answers.minimum();
+
+	        $scope.tags.push(temp);
+	    });
+
+	    mapUserTags($scope.results.userTags);
     }
 
     function parseData(questions, votes){
-    	console.log('votes')
-    	console.log(votes);
 		$scope.chartObject.data = {"cols": [
 		    {id: "t", label: "Topping", type: "string"},
 		    {id: "s", label: "Slices", type: "number"}
@@ -45,23 +63,29 @@ angular.module('iamatApp')
 		    ]}
 		]};	
     }
+    
+    $scope.tags = [];
+    $scope.results = resultsFactory;
 
-    iamatService.getTeams().success(function(data){
-    	$scope.results.teams = data;
-    	console.log(data);
-    }).error(function(){
-    	console.log('ERROR CARGANDO TEAMS');
-    });
 
-   
 	$scope.chartObject = {};
     $scope.chartObject.type = "PieChart";
 
+    var mapUserTags = function(obj) {
+	    $scope.results.userTags = $.map(obj, function(value, index) {
+		    return [value];
+		});
+	};
+
     $scope.triggerChart = function(){
-    	console.log('trigger');
     	googleChartApiPromise.then(parseData($scope.results.answers, $scope.results.userTags[$scope.type]['answers']));
     }
 
-    
+    iamatService.getTeams().success(function(data){
+    	$scope.results.teams = data;
+    	generateTags($scope.results.teams);
+    }).error(function(){
+    	console.log('ERROR CARGANDO TEAMS');
+    });  
 
   });
