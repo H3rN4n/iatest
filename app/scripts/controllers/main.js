b04ce640698cf2ea3f742822d982466085374cad
H3rN4n <desouza030689@gmail.com>
2016-04-08 00:56:08 -0300

    graphics'll changes on select type

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,60 @@
  * Controller of the iamatApp
  */
 angular.module('iamatApp')
-  .controller('MainCtrl', function (iamatService ,resultsFactory) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, iamatService ,resultsFactory,googleChartApiPromise) {
+    
+    $scope.results = resultsFactory;
 
-    console.log(resultsFactory);
+    function getFirst(obj){
+    	for (first in obj) break;
+    	return first
+    }
+
+    
+
+    function buildDataTable(){
+        $scope.chartObject.data = new google.visualization.DataTable();
+        // Continue building the data table.
+    }
+
+    function parseData(questions, votes){
+    	console.log('votes')
+    	console.log(votes);
+		$scope.chartObject.data = {"cols": [
+		    {id: "t", label: "Topping", type: "string"},
+		    {id: "s", label: "Slices", type: "number"}
+		], "rows": [
+		    {c: [
+		        {v: questions[0]['text']},
+		        {v: votes[0]},
+		    ]},
+		    {c: [
+		        {v: questions[1]['text']},
+		        {v: votes[1]}
+		    ]},
+		    {c: [
+		        {v: questions[2]['text']},
+		        {v: votes[2]},
+		    ]}
+		]};	
+    }
 
     iamatService.getTeams().success(function(data){
+    	$scope.results.teams = data;
     	console.log(data);
     }).error(function(){
     	console.log('ERROR CARGANDO TEAMS');
     });
+
+   
+	$scope.chartObject = {};
+    $scope.chartObject.type = "PieChart";
+
+    $scope.triggerChart = function(){
+    	console.log('trigger');
+    	googleChartApiPromise.then(parseData($scope.results.answers, $scope.results.userTags[$scope.type]['answers']));
+    }
+
+    
 
   });
